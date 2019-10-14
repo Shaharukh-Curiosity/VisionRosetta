bfe14858acec97b832904c9faac61d4e83d8e151
Shaharukh-Curiosity <shaharukh.rsp786@gmail.com>
2019-10-14 19:25:03 +0530

    added 404.html

--- dist/prerender.js
+++ dist/prerender.js
@@ -13,7 +13,7 @@ var module_map_ngfactory_loader_1 = require("@nguniversal/module-map-ngfactory-l
 var platform_server_1 = require("@angular/platform-server");
 var static_paths_1 = require("./static.paths");
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-var _a = require('./dist/server/main.bundle'), AppServerModuleNgFactory = _a.AppServerModuleNgFactory, LAZY_MODULE_MAP = _a.LAZY_MODULE_MAP;
+var _a = require('./server/main'), AppServerModuleNgFactory = _a.AppServerModuleNgFactory, LAZY_MODULE_MAP = _a.LAZY_MODULE_MAP;
 var BROWSER_FOLDER = path_1.join(process.cwd(), 'browser');
 // Load the index.html file containing referances to your application bundle.
 var index = fs_1.readFileSync(path_1.join('browser', 'index.html'), 'utf8');
