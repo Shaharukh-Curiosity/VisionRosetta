aff52ce77b2f72016de73ea4d1acbf113a7a1f57
Shaharukh Parmar <shaharukh.rsp786@gmail.com>
2019-12-08 21:32:31 -0500

    made changed in(sitemap, home, add ubuntu tutorials component,create open port tutorials for linux)

---
+++ src/app/ubuntu/Tutorials/detecting-openport/detecting-openport.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detecting-openport',
+  templateUrl: './detecting-openport.component.html',
+  styleUrls: ['./detecting-openport.component.css']
+})
+export class DetectingOpenportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/ubuntu/ubuntu-tut-home/ubuntu-tut-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ubuntu-tut-home',
+  templateUrl: './ubuntu-tut-home.component.html',
+  styleUrls: ['./ubuntu-tut-home.component.css']
+})
+export class UbuntuTutHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
